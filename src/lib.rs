b341a83f99a516f6ea690e2517f6466c0ba967d1
Edwin Jose Palathinkal <edwinhere@gmail.com>
2020-05-27 10:39:54 +0800

    Updated inline documentation

--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,16 @@
 //!  - Multilayer Linkable Spontaneous Anonymous Group (MLSAG) signatures
 //!  - Concise Linkable Spontaneous Anonymous Group (CLSAG) signatures
 //!
-//!  > All blockquotes (except this one) in this documentation are from [Z2M2](https://www.getmonero.org/library/Zero-to-Monero-2-0-0.pdf)
+//! The following scheme has also been implemented from outside [Z2M2](https://www.getmonero.org/library/Zero-to-Monero-2-0-0.pdf):
 //!
-//!  This library is to work with any 512-bit (64 byte output) hashing function. But it uses the
-//!  [Ristretto elliptic curve](https://doc.dalek.rs/curve25519_dalek/ristretto/) for ease of use and better security.
+//!  - [DLSAG: Non-Interactive Refund Transactions For Interoperable Payment Channels in Monero](https://eprint.iacr.org/2019/595.pdf)
 //!
-//!  This library is `#![no_std]` by default so it is possible to compile this library for embedded devices and WebAssembly but we haven't tried.
+//! > All blockquotes (except this one) in this documentation are from [Z2M2](https://www.getmonero.org/library/Zero-to-Monero-2-0-0.pdf)
+//!
+//! This library is designed to work with any 512-bit (64 byte output) hashing function. It uses the
+//! [Ristretto elliptic curve](https://doc.dalek.rs/curve25519_dalek/ristretto/) for ease of use and better security.
+//!
+//! This library is `#![no_std]` by default so it is possible to compile this library for embedded devices and WebAssembly but we haven't tried.
 
 #![no_std]
 #[macro_use]
